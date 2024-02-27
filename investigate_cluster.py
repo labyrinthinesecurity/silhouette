05407690df0492851909392a5e9d8cb645c30d25
Ubuntu <ubuntu@cpavm3.4bvcinbwtz2unfvenr0m1kon4g.ax.internal.cloudapp.net>
2024-02-27 17:13:39 +0100

    RG aware version of silhouette

--- investigate_cluster.py
+++ investigate_cluster.py
@@ -1,4 +1,6 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 from common import *
 
-investigate_cluster(run_partition,"0",verbose=True)
+count,current,desired=investigate_cluster(run_partition,"0",strat=None, verbose=True)
+effort=current-desired
+print("SPNs count",count,"current silhouette",current,"desired",desired,"effort",effort)
