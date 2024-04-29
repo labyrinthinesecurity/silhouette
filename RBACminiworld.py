aac67cc179af0b9b033280f6b0f6259460390bcd
Ubuntu <ubuntu@cpavm3.4bvcinbwtz2unfvenr0m1kon4g.ax.internal.cloudapp.net>
2024-04-29 10:58:01 +0200

    miniworld updates

--- RBACminiworld.py
+++ RBACminiworld.py
@@ -49,7 +49,7 @@ else:
     print("    UNKNOWN")
 s.pop()
 
-print("  * Asymmetry")
+print("  * Non-symmetry")
 # Expected result: UNSAT
 s.push()
 s.add(And(SP(f(a1),f(b1)),Not(SP(f(b1),f(a1)))))
@@ -83,8 +83,7 @@ print("")
 print("  * Find SP-equivalent pairs a,b which are distinct and joined directly by a third pair c distinct from a and b")
 s.add(a2!=b2, a1!=b1, SP(f(a1),f(b1)))
 s.add(Not(And(a1==c1,a2==c2)),Not(And(b1==c1,b2==c2)))
-s.add(Or(And(a2==c2,b2!=c2,f(b1)==f(c1)),And(a2!=c2,b2==c2,f(a1)==f(c1))))
-s.add(Not(And(f(a1)==f(c1),f(b1)==f(c1))))
+s.add(Or(And(a2==c2,b2!=c2,f(b1)==f(c1),f(a1)!=f(c1)),And(a2!=c2,b2==c2,f(a1)==f(c1),f(b1)!=f(c1))))
 result = s.check()
 if result == sat:
     print("    SAT: ", s.model())
@@ -108,7 +107,7 @@ else:
     print("    UNKNOWN")
 s.pop()
 
-print("  * Find SP-equivalent pair (a,b) with no tuple c, distinct from a and b,  joining a and b")
+print("  * Any SP-equivalent pair (a,b) always has a tuple c, distinct from a and b,  joining a and b directly")
 # Expected result: UNSAT
 s.push()
 s.add(Implies(SP(f(a1), f(b1)),ForAll([c1,c2],And(Not(And(a1==c1,a2==c2)),Not(And(b1==c1,b2==c2)),And(Or(a1==c1,b1==c1),Or(a2==c2,b2==c2))))))
@@ -135,24 +134,26 @@ else:
     print("    UNKNOWN")
 s.pop()
 
-TIERBlo=IntVal(10)
-TIERBhi=IntVal(19)
-TIERClo=IntVal(20)
-TIERChi=IntVal(29)
-TIERDlo=IntVal(30)
-TIERDhi=IntVal(39)
-TIERlast=IntVal(40)
-s.add(a1>=TIERBlo,a1<TIERlast)
-s.add(b1>=TIERBlo,b1<TIERlast)
-s.add(c1>=TIERBlo,c1<TIERlast)
+# Define intervals of 3 cylinders CYL10, CYL20 and CYL30
+CYL10_low=IntVal(10)
+CYL10_hi=IntVal(19)
+CYL20_lo=IntVal(20)
+CYL20_hi=IntVal(29)
+CYL30_lo=IntVal(30)
+CYL30_hi=IntVal(39)
 
-# Horizontal (or vertical) Congruence
-s.add(ForAll([a1,b1],Implies(And(a1>=TIERBlo,a1<=TIERBhi,b1>=TIERBlo,b1<=TIERBhi),SP(f(a1), f(b1)))))
-s.add(ForAll([a1,b1],Implies(And(a1>=TIERClo,a1<=TIERChi,b1>=TIERClo,b1<=TIERChi),SP(f(a1), f(b1)))))
-s.add(ForAll([a1,b1],Implies(And(a1>=TIERDlo,a1<=TIERDhi,b1>=TIERDlo,b1<=TIERDhi),SP(f(a1), f(b1)))))
+# Tie a1, b1 and c1 to any of the 3 cylinders
+s.add(a1>=CYL10_low,a1<CYL30_hi)
+s.add(b1>=CYL10_low,b1<CYL30_hi)
+s.add(c1>=CYL10_low,c1<CYL30_hi)
+
+# Horizontal Congruence: if two scopes belong to the same cylinder, they are SP equivalent
+s.add(ForAll([a1,b1],Implies(And(a1>=CYL10_low,a1<=CYL10_hi,b1>=CYL10_low,b1<=CYL10_hi),SP(f(a1), f(b1)))))
+s.add(ForAll([a1,b1],Implies(And(a1>=CYL20_lo,a1<=CYL20_hi,b1>=CYL20_lo,b1<=CYL20_hi),SP(f(a1), f(b1)))))
+s.add(ForAll([a1,b1],Implies(And(a1>=CYL30_lo,a1<=CYL30_hi,b1>=CYL30_lo,b1<=CYL30_hi),SP(f(a1), f(b1)))))
 
 print("")
-print("Part 3/Prove that HV congruence is an equivalence relation")
+print("Part 3/Prove that H congruence is an equivalence relation")
 print("  * Non-reflexivity")
 # Expected result: UNSAT
 s.push()
@@ -166,7 +167,7 @@ else:
     print("    UNKNOWN")
 s.pop()
 
-print("  * Asymmetry")
+print("  * Non-symmetry")
 # Expected result: UNSAT
 s.push()
 s.add(And(SP(f(a1),f(b1)),Not(SP(f(b1),f(a1)))))
@@ -195,11 +196,10 @@ s.pop()
 s.push()
 print("")
 
-print("  * Find HV-equivalent pairs a,b which are distinct and joined by a third pair c distinct from a and b")
+print("  * Find H-equivalent pairs a,b which are distinct and joined by a third pair c distinct from a and b")
 s.add(a2!=b2, a1!=b1, SP(f(a1),f(b1)))
 s.add(Not(And(a1==c1,a2==c2)),Not(And(b1==c1,b2==c2)))
-s.add(Or(And(a2==c2,b2!=c2,f(b1)==f(c1)),And(a2!=c2,b2==c2,f(a1)==f(c1))))
-s.add(Not(And(f(a1)==f(c1),f(b1)==f(c1))))
+s.add(Or(And(a2==c2,b2!=c2,f(b1)==f(c1),f(a1)!=f(c1)),And(a2!=c2,b2==c2,f(a1)==f(c1),f(b1)!=f(c1))))
 result = s.check()
 if result == sat:
     print("    SAT: ", s.model())
@@ -209,7 +209,7 @@ else:
     print("    UNKNOWN")
 s.pop()
 
-print("  * Find HV-equivalent pair (a,b) with no tuple c, distinct from a and b,  joining a and b")
+print("  * Any H-equivalent pairs (a,b) has a tuple c, distinct from a and b,  joining a and b directly")
 # Expected result: UNSAT
 s.push()
 s.add(Implies(SP(f(a1), f(b1)),ForAll([c1,c2],And(Not(And(a1==c1,a2==c2)),Not(And(b1==c1,b2==c2)),And(Or(a1==c1,b1==c1),Or(a2==c2,b2==c2))))))
@@ -223,7 +223,7 @@ else:
 s.pop()
 s.push()
 print("")
-print("  * Find not HV-equivalent pairs")
+print("  * Find not H-equivalent pairs")
 # Expected result: UNSAT
 s.add(Not(SP(f(a1),f(b1))))
 result = s.check()
